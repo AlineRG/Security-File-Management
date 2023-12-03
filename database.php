03f69e7bc23ad6618d56b258b30169e776db2664
AlineRG <134813655+AlineRG@users.noreply.github.com>
2023-12-02 21:21:51 -0500

    Update database.php
    $conn = new PDO('mysql:host=localhost;dbname=animal_registration;port=3308', 'root', ''); 
    
    Delete the port that I use to make to the code works in my computer.

--- database.php
+++ database.php
@@ -1,7 +1,7 @@
 <?php
 
 try {
-    $conn = new PDO('mysql:host=localhost;dbname=animal_registration;port=3308', 'root', '');
+    $conn = new PDO('mysql:host=localhost;dbname=animal_registration','root','');
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     echo "Connection failed: " . $e->getMessage();
